92e46d906be372b6b6bf5925a8e4e7627372f664
satriawarn <satriawarn@gmail.com>
2022-07-13 08:25:31 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:foodpedia/recipe.dart';
+import 'package:foodpedia/recipe_detail.dart';
+
+void main() {
+  runApp(const FoodPedia());
+}
+
+class FoodPedia extends StatelessWidget {
+  const FoodPedia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+      title: 'FoodPedia',
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.grey,
+          secondary: Colors.black,
+        ),
+      ),
+      home: const HomePage(title: 'FoodPedia'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+            itemCount: Recipe.samples.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return RecipeDetail(recipe: Recipe.samples[index]);
+                  }));
+                },
+                child: buildRecipeCard(Recipe.samples[index]),
+              );
+            }),
+      ),
+    );
+  }
+}
+
+Widget buildRecipeCard(Recipe recipe) {
+  return Card(
+    elevation: 2.0,
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          const SizedBox(
+            height: 14.0,
+          ),
+          Text(
+            recipe.label,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Poppins',
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
