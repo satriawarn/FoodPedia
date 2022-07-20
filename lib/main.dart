2e9db08c49801520f8ac473b9f2df3fedf99bbc5
satriawarn <satriawarn@gmail.com>
2022-07-20 09:19:17 +0700

    Update Shared Preferences Able to save data in app using shared preferences, the async/await keyword pair let run asynchronous code off the main ui thread, shared preferences not recommended for save sensitive data use flutter_secure_storage instead.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:foodpedia/recipe.dart';
-import 'package:foodpedia/recipe_detail.dart';
+import 'package:foodpedia/ui/main_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const FoodPedia());
 }
 
@@ -11,84 +11,17 @@ class FoodPedia extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = ThemeData();
     return MaterialApp(
       title: 'FoodPedia',
-      theme: theme.copyWith(
-        colorScheme: theme.colorScheme.copyWith(
-          primary: Colors.grey,
-          secondary: Colors.black,
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const HomePage(title: 'FoodPedia'),
+      home: const MainScreen(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SafeArea(
-        child: ListView.builder(
-            itemCount: Recipe.samples.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return RecipeDetail(recipe: Recipe.samples[index]);
-                  }));
-                },
-                child: buildRecipeCard(Recipe.samples[index]),
-              );
-            }),
-      ),
-    );
-  }
-}
-
-Widget buildRecipeCard(Recipe recipe) {
-  return Card(
-    elevation: 2.0,
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-    child: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: <Widget>[
-          Image(image: AssetImage(recipe.imageUrl)),
-          const SizedBox(
-            height: 14.0,
-          ),
-          Text(
-            recipe.label,
-            style: const TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.w700,
-              fontFamily: 'Poppins',
-            ),
-          )
-        ],
-      ),
-    ),
-  );
-}
---
+++ lib/ui/main_screen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:foodpedia/ui/recipes/recipe_list.dart';
+import 'package:foodpedia/ui/shopping/shopping_list.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'colors.dart';
+import 'myrecipes/my_recipe_list.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  List<Widget> pageList = <Widget>[];
+  static const String prefSelectedIndexKey = 'selectedIndex';
+
+  @override
+  void initState() {
+    super.initState();
+    pageList.add(const RecipeList());
+    pageList.add(const MyRecipeList());
+    pageList.add(const ShoppingList());
+    getCurrentIndex();
+  }
+
+  void saveCurrentIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setInt(prefSelectedIndexKey, _selectedIndex);
+  }
+
+  void getCurrentIndex() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey(prefSelectedIndexKey)) {
+      setState(() {
+        final index = prefs.getInt(prefSelectedIndexKey);
+        if (index != null) {
+          _selectedIndex = index;
+        }
+      });
+    }
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    saveCurrentIndex();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String title;
+    switch (_selectedIndex) {
+      case 0:
+        title = 'Recipes';
+        break;
+      case 1:
+        title = 'Bookmarks';
+        break;
+      case 2:
+        title = 'Groceries';
+        break;
+      default:
+        title = 'Recipes';
+        break;
+    }
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/images/icon_recipe.svg',
+              color: _selectedIndex == 0 ? green : Colors.grey,
+              semanticsLabel: 'Recipes',
+            ),
+            label: 'Recipes',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/images/icon_bookmarks.svg',
+              color: _selectedIndex == 1 ? green : Colors.grey,
+              semanticsLabel: 'Bookmarks',
+            ),
+            label: 'Bookmarks',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/images/icon_shopping_list.svg',
+              color: _selectedIndex == 2 ? green : Colors.grey,
+              semanticsLabel: 'Groceries',
+            ),
+            label: 'Groceries',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: green,
+        onTap: _onItemTapped,
+      ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          systemNavigationBarColor: Colors.white,
+          statusBarColor: Colors.white,
+          statusBarBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.dark,
+          systemNavigationBarDividerColor: Colors.white,
+          systemNavigationBarIconBrightness: Brightness.light,
+        ),
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: pageList,
+      ),
+    );
+  }
+}
